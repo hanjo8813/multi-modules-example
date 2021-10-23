0a00d97c97a902f437baa5a947b44c3f6dcf6c71
한재원 <hanjo8813@gmail.com>
2021-10-24 05:31:23 +0900

    init project

---
+++ src/main/java/com/example/multimodulesexample/MultiModulesExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimodulesexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModulesExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModulesExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/multimodulesexample/MultiModulesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multimodulesexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModulesExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
